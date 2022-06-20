731e7a3ccf93eb962535158cc3c8bdac18c5bb2b
DanijelaMilekic <105737357+DanijelaMilekic@users.noreply.github.com>
2022-06-20 20:53:12 +0200

    Main
    Kreirane su tabele za Zaposlene, Softvere, Rendere i Cetkice. Korisceni su napravljeni modeli. Promenjena je velicina prozora na 3/4 kao sto je zahtevano u tekstu zadatka. Osposobljene su funkcionalnosti New i Open. Odradjen je refresh.

--- Projekat/src/gui/JMain.java
+++ Projekat/src/gui/JMain.java
@@ -11,6 +11,7 @@ import java.awt.event.KeyEvent;
 import java.util.Date;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
@@ -18,18 +19,36 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
-
+import javax.swing.table.TableModel;
 
 import crud.BootCrud;
+import crud.CetkicaCrud;
+import crud.RenderCrud;
+import crud.SoftverCrud;
+import crud.ZaposleniCrud;
+import gui.cetkica.CetkicaTable;
+import gui.cetkica.JCreateCetkica;
+import gui.render.JCreateRender;
+import gui.render.RenderiTable;
+import gui.softver.JCreateSoftver;
+import gui.softver.SoftveriTable;
+import gui.zaposleni.JCreateZaposleni;
+import gui.zaposleni.ZaposleniTable;
 import util.Formating;
 
-public class JMain {
-	
-	private JFrame frame;
+public class JMain implements Refreshable {
 
+	private JFrame frame;
+	private JTable tableZaposleni;
+	private JTable tableSoftveri;
+	private JTable tableRenderi;
+	private JTable tableCetkice;
+	private Refreshable thisRefreshable = this;
 
 	/**
 	 * Launch the application.
@@ -62,7 +81,7 @@ public class JMain {
 		frame = new JFrame();
 		frame.setTitle("Naslov");
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		frame.setSize(dimension.width * 2/4, dimension.height * 2/4);
+		frame.setSize(dimension.width * 3/4, dimension.height * 3/4);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
@@ -92,7 +111,25 @@ public class JMain {
 //		tabbedPane.addTab(null, tabbedPane)
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
+		tableZaposleni = new JTable();
+		tableZaposleni.setModel(new ZaposleniTable(ZaposleniCrud.getAllZaposlenis()));
+		tableZaposleni.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tabbedPane.addTab("Zaposleni", null, tableZaposleni, null);
 		
+		tableSoftveri = new JTable();
+		tableSoftveri.setModel(new SoftveriTable(SoftverCrud.getAllSoftveri()));
+		tableSoftveri.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tabbedPane.addTab("Softveri", null, tableSoftveri, null);
+		
+		tableRenderi = new JTable();
+		tableRenderi.setModel(new RenderiTable(RenderCrud.getAllRenderi()));
+		tableSoftveri.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tabbedPane.addTab("Renderi", null, tableRenderi, null);
+		
+		tableCetkice = new JTable();
+		tableCetkice.setModel(new CetkicaTable(CetkicaCrud.getAllCetkice()));
+		tableCetkice.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tabbedPane.addTab("Cetkice", null, tableCetkice, null);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
@@ -102,12 +139,60 @@ public class JMain {
 		menuBar.add(fileMenu);
 		
 		JMenuItem newFileMenuItem = new JMenuItem("New");
+		newFileMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JDialog dialog;
+				TableModel table = ((JTable) tabbedPane.getSelectedComponent()).getModel();
+				if (table instanceof ZaposleniTable) {
+					dialog = new JCreateZaposleni(thisRefreshable);
+				} else if (table instanceof SoftveriTable) {
+					dialog = new JCreateSoftver(thisRefreshable);
+				} else if (table instanceof CetkicaTable) {
+					dialog = new JCreateCetkica(thisRefreshable);
+				} else {
+					dialog = new JCreateRender(thisRefreshable);
+				}
+				dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				dialog.setVisible(true);
+			}
+		});
+		newFileMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
 		fileMenu.add(newFileMenuItem);
 		
 		JMenu openFileSubmenu = new JMenu("Open");
 		fileMenu.add(openFileSubmenu);
 		
+		JMenuItem openZaposleniMenuItem = new JMenuItem("Zaposleni");
+		openZaposleniMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedComponent(tableZaposleni);
+			}
+		});
+		openFileSubmenu.add(openZaposleniMenuItem);
 		
+		JMenuItem openSoftverMenuItem = new JMenuItem("Softver");
+		openSoftverMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedComponent(tableSoftveri);
+			} 
+		});
+		openFileSubmenu.add(openSoftverMenuItem); 
+		
+		JMenuItem openRenderMenuItem = new JMenuItem("Render"); 
+		openRenderMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedComponent(tableRenderi);
+			}
+		});
+		openFileSubmenu.add(openRenderMenuItem); 
+		
+		JMenuItem openCetkicaMenuItem = new JMenuItem("Cetkica");
+		openCetkicaMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedComponent(tableCetkice);
+			}
+		});
+		openFileSubmenu.add(openCetkicaMenuItem);
 		
 		
 		JMenuItem exitFileMenuItem = new JMenuItem("Exit");
@@ -141,5 +226,14 @@ public class JMain {
 		
 		
 	}
-
+	
+	@Override
+	public void refresh(JDialog dialog) {
+		tableZaposleni.setModel(new ZaposleniTable(ZaposleniCrud.getAllZaposlenis()));	
+		tableSoftveri.setModel(new SoftveriTable(SoftverCrud.getAllSoftveri()));
+		tableRenderi.setModel(new RenderiTable(RenderCrud.getAllRenderi()));
+		tableCetkice.setModel(new CetkicaTable(CetkicaCrud.getAllCetkice()));
+		dialog.setVisible(false);
+		dialog.dispose();
+	}
 }
