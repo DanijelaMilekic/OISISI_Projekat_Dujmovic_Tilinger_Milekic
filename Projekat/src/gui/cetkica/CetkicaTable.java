d734718460bd70c24e035d04dd942c7d514d77d5
LanaDujmovic <lana.dujmovic.ns@gmail.com>
2022-06-22 06:52:17 +0200

    Ispravka CetkicaTable
    U prethodnom je nije dodata kolona za boju. Sada umesto dve kolone (naziv, namena), tabla sa cetkicama ima i trecu koja vraca podatak o boji, odnosno obojeno polje.

--- Projekat/src/gui/cetkica/CetkicaTable.java
+++ Projekat/src/gui/cetkica/CetkicaTable.java
@@ -1,25 +1,27 @@
 package gui.cetkica;
 
+import java.awt.Color;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
 import model.Cetkica;
 
-public class CetkicaTable extends AbstractTableModel  {
+public class CetkicaTable extends AbstractTableModel {
 
 	
+	private static final long serialVersionUID = -7870870896455561500L;
 	
 	private List<Cetkica> cetkice;
 
 	public CetkicaTable(List<Cetkica> cetkice) {
 		this.cetkice = cetkice;
 	}
-
+	
 	public void setCetkice(List<Cetkica> cetkice) {
 		this.cetkice = cetkice;
 	}
-	
+
 	@Override
 	public int getRowCount() {
 		return cetkice.size();
@@ -27,7 +29,7 @@ public class CetkicaTable extends AbstractTableModel  {
 
 	@Override
 	public int getColumnCount() {
-		return 2;
+		return 3;
 	}
 
 	@Override
@@ -38,6 +40,9 @@ public class CetkicaTable extends AbstractTableModel  {
 		}
 		case 1: {
 			return "Namena";
+		}
+		case 2: {
+			return "Boja";
 		}
 		}
 		return null;
@@ -54,9 +59,21 @@ public class CetkicaTable extends AbstractTableModel  {
 		case 1: {
 			return cetkice.get(rowIndex).getNamena();
 		}
+		case 2: {
+			return "";
+		}
 		}
 		return null;
 	}
+	
+	public Cetkica getRowValue(int i) {
+		return cetkice.get(i);
+	}
+	
+	public Color getColor(int i) { // dodatno za prikaz obojenog polja
+		return cetkice.get(i).getBoja();
+	}
+	
 }
 
 
